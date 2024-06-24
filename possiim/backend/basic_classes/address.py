1ffbb4bed3dd79925d6b22321393d78c5f1cee8f
Tepeset <lnorris0987@gmail.com>
2024-06-24 14:51:34 -0700

    upload basic files
    Starting somewhere...upload of basic folder structure, getting things somewhat laid out and organized. Began some basic backend coding.

---
+++ possiim/backend/basic_classes/address.py
@@ -0,0 +1,11 @@
+class Address:
+    def __init__(self, country:str, state:str, city:str, street:str, postal:int):
+        self.country = country
+        self.state = state
+        self.city = city
+        self.street = street
+        self.postal = postal
+
+        def get_address(self):
+            self.get_address
+            return f"{street}, {city}, {state}, {postal}, {country}"
